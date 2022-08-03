e073333d3a6c697e03d3dea8774a0a8f65e019ed
andrei krug <andreigurk@gmail.com>
2022-08-03 10:28:56 -0300

    commit de hashcode

---
+++ Estudos/src/App.java
@@ -0,0 +1,12 @@
+import hashCode.Produto;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Produto p1 = new Produto("arroz", 20);
+        Produto p2 = new Produto("arroz", 20);
+
+        System.out.println(p1.equals(p2));
+    }
+}
---
+++ Estudos/src/hashCode/Produto.java
@@ -0,0 +1,13 @@
+package hashCode;
+
+public class Produto 
+{
+    private String nome;
+    private int preco;
+
+    public Produto(String nome, int preco)
+    {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
