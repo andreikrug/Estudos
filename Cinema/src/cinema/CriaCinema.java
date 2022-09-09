82828989e3ab6a1a89fae1bdc8bd97579590685d
Andrei Krug <ANDREIKRUG@edu.unisinos.br>
2022-09-09 20:58:56 -0300

    adicionando projeto cinema

---
+++ Cinema/src/cinema/CriaCinema.java
@@ -0,0 +1,85 @@
+
+
+package cinema;
+
+public class CriaCinema 
+{
+    Assentos[] lugares;
+    String[] letras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
+    
+    public CriaCinema(int quant)
+    {
+        lugares = new Assentos[quant];
+    }
+    
+    public void criaLugares()
+    {   
+        int val1 = 1;
+        int val2 = 0;
+        
+        for(int i = 0; i<lugares.length; i++)
+        {   
+            
+            if(val1  < 15)
+            {
+                lugares[i] = new Assentos(letras[val2]+val1, false);
+                val1 ++;
+                
+              
+                
+            } else {
+                i--;
+                val2++;
+                val1 = 1;
+                
+                
+            }
+        }
+    }
+    
+    public void ListaLugaresVagos()
+    {   
+        for(Assentos a : lugares)
+        {
+            
+                System.out.println(a.toString());
+            
+        }
+    }
+    
+    public boolean reservaLugar(String lugar)
+    {
+        for(Assentos a : lugares)
+        {
+            if(a.getLugar().equalsIgnoreCase(lugar))
+            {
+                if(!a.isReservado())
+                {  
+                    System.out.println("Lugar vago e vai ser reservado para você");
+                    a.setReservado(true);
+                    return true;
+                    
+                } else {
+                    System.out.println("Lugar já está reservado, procure reservar outro");
+                    return false;
+                }
+            }
+        }
+        return false;
+      
+    }
+    
+    public void totalDeLugaresOcupados()
+    {
+        int val = 0;
+        for(Assentos a : lugares){
+            if(a.isReservado())
+            {
+                val ++;
+            }
+        }
+        System.out.println("A quantidade de lugares ocupados é: "+val+" de 168");
+    }
+            
+    
+}
