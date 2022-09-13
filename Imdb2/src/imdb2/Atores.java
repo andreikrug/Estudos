348f4e0fa75934479b4ae7ff5000fce58c486906
Andrei Krug <ANDREIKRUG@edu.unisinos.br>
2022-09-12 21:18:14 -0300

    adicionando projeto imdb2

---
+++ Imdb2/src/imdb2/Atores.java
@@ -0,0 +1,18 @@
+
+package imdb2;
+
+public class Atores 
+{
+    private String nome;
+    public Atores(String p_nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ Imdb2/src/imdb2/Cinema.java
@@ -0,0 +1,82 @@
+
+package imdb2;
+
+
+public class Cinema 
+{
+     private int codigo;
+    private String nomeFilme;
+    private int anoFilme;
+    private String diretorFilme;
+    
+    private Atores[] atores = new Atores[2];
+    private char tipo;
+
+    public Cinema(int codigo, String nomeFilme, int anoFilme, String diretorFilme,  char tipo) 
+    {
+        this.codigo = codigo;
+        this.nomeFilme = nomeFilme;
+        this.anoFilme = anoFilme;
+        this.diretorFilme = diretorFilme;
+        
+        this.tipo = tipo;
+    }
+    
+    public boolean insereAtor(Atores a){
+        for(Atores at : atores){
+            if(at == null){
+                at = a;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNomeFilme() {
+        return nomeFilme;
+    }
+
+    public void setNomeFilme(String nomeFilme) {
+        this.nomeFilme = nomeFilme;
+    }
+
+    public int getAnoFilme() {
+        return anoFilme;
+    }
+
+    public void setAnoFilme(int anoFilme) {
+        this.anoFilme = anoFilme;
+    }
+
+    public String getDiretorFilme() {
+        return diretorFilme;
+    }
+
+    public void setDiretorFilme(String diretorFilme) {
+        this.diretorFilme = diretorFilme;
+    }
+
+    public Atores[] getAtores() {
+        return atores;
+    }
+
+    public void setAtores(Atores[] atores) {
+        this.atores = atores;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+}
